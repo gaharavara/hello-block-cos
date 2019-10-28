4403b33b532415f86b8bedce560e21d1a473ce22
AsociTon <asociton@outlook.com>
2019-10-28 23:00:35 +0530

    Shift to express and add purchase and home page.
    Adding assets used in the game to decorate the home and purchase page.
    To work on deriving data from the get requests.

--- index.js
+++ index.js
@@ -1,96 +1,25 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 const path = require('path');
+const router = express.Router();
 
-const server = http.createServer((req, res)=>{
+app.use(express.static(__dirname + '/public'));
 
-
-// This is the most basic approach and is not much effective for bigger operations
-/*
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname,'public','index.html'), (err, content)=>{
-                if (err) throw err;
-                res.writeHead(200, { 'Content-Type':'text/html'});
-                res.end(content);
-            }
-        );
-    }
-
-    if (req.url === '/api/users') {
-        const users = [
-            { name: 'Abhi', age: 19},
-            { name: 'kushal', age: 21}
-        ];
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
-    }
-*/
-   if (req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        fs.createReadStream("./public/transact.html", "UTF-8").pipe(res);
-    }
-
-    // Build file path
-    let filePath = path.join(
-        __dirname,
-        'public',
-        req.url === '/' ? 'index.html' : req.url
-    );
-    
-    // Extension of the file
-    let extname = path.extname(filePath);
-
-    // Initial content type
-    let contentType = 'text/html';
-
-    // Check ext and set content type
-    switch(extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;                    
-
-    }
-
-    // Read File
-    fs.readFile(filePath, (err, content) => {
-        if(err){
-            if(err.code == 'ENOENT') {
-                // Page not found
-                fs.readFile(path.join(__dirname,'public','404.html'), (err, content) => {
-                    res.writeHead(200, {'Content-Type':'text/html'});
-                    res.end(content, 'utf8');
-
-                });
-            } else {
-                // Some server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else {
-            // Success
-            res.writeHead(200, {'Content-Type': contentType});
-            res.end(content, 'utf8');
-        }
-
-    });
-
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
-// Takes the port assigned to the enviornment path variable or else 7000 if it is free
-const PORT = process.env.PORT || 7000;
+router.get('/transact',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/transact.html'));
+});
 
-server.listen(PORT, ()=>{});
+router.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/transact.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 7000);
+
+console.log('Running at Port 3000');
